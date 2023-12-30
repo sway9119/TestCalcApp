2c6078bf2e835deb0657fb7c63486391fed8cec5
sway <40759948+sway9119@users.noreply.github.com>
2023-12-30 19:16:49 +0900

    計算画面の入力バリデーションを追加

--- app/src/main/java/com/example/testcalcapp/MainActivity.kt
+++ app/src/main/java/com/example/testcalcapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.testcalcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.calculate)
+
+        button.setOnClickListener() {
+            var isValid = true
+            val priceEditText = findViewById<EditText>(R.id.price)
+            val priceText = priceEditText.text.toString()
+
+            if (priceText.isEmpty()) {
+                // 定価が未入力
+                priceEditText.error = getString(R.string.price_error)
+                isValid = false
+            }
+
+            val discountEditText = findViewById<EditText>(R.id.discount)
+            val discountText = discountEditText.text.toString()
+
+            if (discountText.isEmpty()) {
+                // 割引率が未入力
+                discountEditText.error = getString(R.string.discount_error)
+                isValid = false
+            }
+
+            if (isValid) {
+                // 文字列を整数型に変換
+                val price = Integer.parseInt(priceText)
+                val discount = Integer.parseInt(discountText)
+
+                // TODO: 計算結果画面への画面繊維する
+            }
+        }
     }
 }
